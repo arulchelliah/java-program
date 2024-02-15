ab196775350a51ab50e74283f5d4f8b42a141a46
arulchelliah <155653435+arulchelliah@users.noreply.github.com>
2024-02-15 20:43:25 +0530

    Add files via upload

---
+++ Pattern 2.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Main
+  {
+    public static void main(String args[])
+    {
+      Scanner d=new Scanner(System.in);
+      int i,j,n;
+      n=d.nextInt();
+      for(i=n;i>=1;i--)
+        {
+          for(j=1;j<=i;j++)
+            {
+              System.out.print(i+" ");
+            }
+          System.out.println();
+        }
+    }
+  }
---
+++ pattern 1.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Pattern2
+{
+  public static void main(String args[])
+  {
+    Scanner d=new Scanner(System.in);
+    int n,i,j;
+    n=d.nextInt();
+    for(i=n;i>=1;i--)
+      {
+        for(j=n;j>=i;j--)
+          {
+            System.out.print(j);
+          }
+        System.out.println();
+      }
+  }
+}
